bfd97f7b05b2809e6d93e7f8e03e7df71d4bcd1a
doridoro <dorothea.reher@gmail.com>
2023-07-07 18:49:20 +0200

    change register form, to display the username, email and twice password

--- litrevu/accounts/forms.py
+++ litrevu/accounts/forms.py
@@ -1,23 +1,27 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import ButtonHolder, Fieldset, Layout, Submit
 
 from .models import User
 
 
-class RegisterForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password1', 'password2']
 
     @property
     def helper(self):
         helper = FormHelper()
         helper.layout = Layout(
             Fieldset(
-                "Your unique username",
+                "Register here",
                 'username',
+                'email',
+                'password1',
+                'password2',
             ),
             ButtonHolder(
                 Submit('submit', "Register")
