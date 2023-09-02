d74fa4e6c07cd998f5f4a7b8482c9d88c8c85ccd
doridoro <dorothea.reher@gmail.com>
2023-09-02 15:57:51 +0200

    started button 'Create a review' lower on feeds_page

--- litrevu/review/urls.py
+++ litrevu/review/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from .views import FeedsView, CreateTicketView, CreateReviewView
+from .views import (
+    FeedsView,
+    CreateTicketView,
+    CreateReviewView,
+    CreateReviewForTicketView,
+)
 
 app_name = "review"
 
@@ -8,4 +13,9 @@ urlpatterns = [
     path("feeds/", FeedsView.as_view(), name="feeds_page"),
     path("create_ticket/", CreateTicketView.as_view(), name="create_ticket"),
     path("create_review/", CreateReviewView.as_view(), name="create_review"),
+    path(
+        "create_review_ticket/<int:pk>/",
+        CreateReviewForTicketView.as_view(),
+        name="create_review_ticket",
+    ),
 ]
--- litrevu/review/views.py
+++ litrevu/review/views.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView
 from django.views.generic.base import TemplateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 from .forms import TicketForm, ReviewForm
 from .models import Ticket, Review
 
 
 class FeedsView(LoginRequiredMixin, TemplateView):
-    template_name = "feeds_page.html"
+    """All reviews and tickets on the feeds page"""
+
+    template_name = "feeds/feeds_page.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -18,9 +20,11 @@ class FeedsView(LoginRequiredMixin, TemplateView):
 
 
 class CreateTicketView(LoginRequiredMixin, CreateView):
+    """Button: 'Ask for a review' View. Create a new ticket"""
+
     model = Ticket
     form_class = TicketForm
-    template_name = "create_ticket_page.html"
+    template_name = "feeds/create_ticket_page.html"
     success_url = reverse_lazy("review:feeds_page")
 
     def form_valid(self, form):
@@ -33,9 +37,14 @@ class CreateTicketView(LoginRequiredMixin, CreateView):
 
 
 class CreateReviewView(LoginRequiredMixin, CreateView):
+    """Button: 'Create a review' on the top of the page. You choose a ticket to review."""
+
+    model = Review
     form_class = ReviewForm
-    template_name = "create_review_page.html"
+    template_name = "feeds/create_review_page.html"
     success_url = reverse_lazy("review:feeds_page")
+
+    # TODO: display just the tickets without a review attached!
 
     def form_valid(self, form):
         review = form.save(commit=False)
@@ -43,3 +52,29 @@ class CreateReviewView(LoginRequiredMixin, CreateView):
         form.save()
 
         return super().form_valid(form)
+
+
+class CreateReviewForTicketView(LoginRequiredMixin, CreateView):
+    """Button 'Create a review' on a ticket without a review."""
+
+    # TODO: display the image of ticket
+    # TODO: remove ticket ChoiceField from ReviewForm
+
+    model = Review
+    form_class = ReviewForm
+    template_name = "feeds/create_review_for_ticket.html"
+    success_url = reverse_lazy("review:feeds_page")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["ticket"] = Ticket.objects.get(pk=self.kwargs.get("pk"))
+
+        return context
+
+    def form_valid(self, form):
+        print("here---", form.instance.ticket_id)
+        form.instance.ticket_id = self.kwargs["pk"]
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("create_review_ticket", kwargs={"pk": self.object.ticket.pk})
