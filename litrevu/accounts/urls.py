23f92c902babb89ab5dac0396203540b10442176
doridoro <dorothea.reher@gmail.com>
2023-09-03 11:52:02 +0200

    working on follow/unfollow on apo page, still not working

--- litrevu/accounts/urls.py
+++ litrevu/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import RegisterView, AboView
+from .views import RegisterView, AboFollowView, AboUnfollowView
 
 app_name = "accounts"
 
@@ -9,5 +9,5 @@ urlpatterns = [
     path("", LoginView.as_view(template_name="login_page.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
-    path("abo/", AboView.as_view(), name="abo_page"),
+    path("abo/", AboFollowView.as_view(), name="abo_page"),
 ]
--- litrevu/accounts/views.py
+++ litrevu/accounts/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic.edit import FormView, CreateView
+from django.views.generic.edit import FormView, CreateView, UpdateView
 
 from .forms import RegisterForm, AboForm
 from .models import UserFollows, User
@@ -21,27 +22,14 @@ class RegisterView(CreateView):
         return super().form_valid(form)
 
 
-# class RegisterView(FormView):
-#     form_class = RegisterForm
-#     template_name = "register.html"
-#     success_url = reverse_lazy("review:review_page")
-#
-#     def form_valid(self, form):
-#         user = form.save()
-#
-#         if user:
-#             login(self.request, user)
-#
-#         return super().form_valid(form)
-
-
-class AboView(LoginRequiredMixin, FormView):
-    """View for the abo page. Follow a user, lists all followed users"""
+# TODO: not working
+class AboFollowView(LoginRequiredMixin, FormView):
+    """View for the abo page. Follow a user"""
 
     model = UserFollows
     form_class = AboForm
     template_name = "abo_page.html"
-    # success_url = reverse_lazy("accounts:abo_page")
+    success_url = reverse_lazy("accounts:abo_page")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -63,23 +51,42 @@ class AboView(LoginRequiredMixin, FormView):
 
         return context
 
-    # TODO: not working
     def form_valid(self, form):
-        if "follow" in self.request.POST:
-            to_be_followed_user = form.cleaned_data["search"]
-            user_to_follow = User.objects.get(username=to_be_followed_user)
-            UserFollows.objects.create(
+        print("here-----", form.data)
+        if "follow" in form.data:
+            user_to_follow = get_object_or_404(
+                User, username=form.cleaned_data["search"]
+            )
+            user_follows = UserFollows(
                 user=self.request.user, followed_user=user_to_follow
             )
-
-        elif "unfollow" in self.request.POST:
-            user_id = self.request.POST.get("unfollow")
-            user_to_unfollow = User.objects.get(id=user_id)
-            UserFollows.objects.get(
-                user=self.request.user, followed_user=user_to_unfollow
-            ).delete()
+            user_follows.save()
 
         else:
             return reverse_lazy("accounts:abo_page")
 
         return super().form_valid(form)
+
+
+class AboUnfollowView(LoginRequiredMixin, UpdateView):
+    """View to unfollow a user and list the users how are following the user."""
+
+    model = UserFollows
+    fields = ["user"]
+    # template_name = "abo_page_unfollow.html"
+    success_url = reverse_lazy("accounts:abo_page")
+
+    def get_context_data(self, **kwargs):
+        print("lets see -------", self.object)
+
+    """
+            elif "unfollow" in form.data:
+            print("unfollow", form.data)
+            user_to_unfollow = get_object_or_404(User, id=form.data.get("unfollow"))
+            UserFollows.objects.get(
+                user=self.request.user, followed_user=user_to_unfollow
+            ).delete()
+
+            if form.error:
+                print("errors", form.errors)
+    """
